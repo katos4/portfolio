d096cf5ceae98e93d8293c19ca8b257a2074883b
Grego <gregoharriero@gmail.com>
2021-03-10 12:36:53 +0100

    modificacion index api para https

--- index.js
+++ index.js
@@ -1,18 +1,30 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var https = require('https');
+var fs = require('fs');
 var app = require('./app');
 var port = process.env.PORT || 3700;
 
 mongoose.Promise =  global.Promise;
-mongoose.connect('mongodb://localhost:27017/portfolio', { useMongoClient: true })
+mongoose.connect('mongodb://localhost:27017/portfolio')
         .then(() => {
             console.log('Conexion a la base de datos establecida');
 
-            // Creacion del servidor
-            app.listen(port, () => {
-                console.log('Servidor corriendo correctamente en: localhost:3700');
+            //crear servidor https
+            https.createServer(
+                {
+                key: fs.readFileSync('privkey.pem'),
+                cert: fs.readFileSync('cert.pem'),
+                }, 
+                app
+            ).listen(port, () => {
+                console.log('Servidor HTTPs corriendo en: localhost:3700');
             });
+            // Creacion del servidor http normal
+           /* app.listen(port, () => {
+                console.log('Servidor HTTP corriendo en: localhost:3700');
+            });*/
 
         })
         .catch(err => {
